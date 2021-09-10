0850638d1ec6e8a35a1af225102f8f111fd1b710
Alijk <alijafarkazemi@gmail.com>
2021-09-11 01:24:49 +0430

    Add run.bat and update run.s

---
+++ run.bat
@@ -0,0 +1,15 @@
+::
+:: This script is used for windows operating systems
+::
+
+::
+:: Varibles
+::
+set JAR=paper-1-12-2.jar
+set MEMORY=1G
+
+
+::
+:: JVM Commands
+::
+java -Xms%MEMORY% -Xmx%MEMORY% -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar %JAR% nogui
