4e6ffe54ec4bddd39da23ddf50a70a90c2e79a8e
xii69 <87283583+xii69@users.noreply.github.com>
2022-05-16 00:53:42 +0430

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,10 @@
-#
-# This script is used for bash based operating systems
-# You'll need screen package (If not installed) installed on yor machine
-#
+#!/bin/bash
 
-#
-# Varibles
-#
-JAR="spigot-1-12-2.jar"
-SCREENNAME="eggwars"
-MEMORY="512M"
+SCREEN="Server"
+JAR="Server.jar"
+RAM="1024M"
+JAVA="Java-Path"
+FLAGS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true -Dusing.aikars.flags=https://mcflags.emc.gs"
 
-#
-# JVM Commands
-#
-COMMAND="java -Xms$MEMORY -Xmx$MEMORY -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $JAR nogui"
-
-#
-# Screen Startup
-#
-echo "Starting server $SCREENNAME"
-screen -dmS $SCREENNAME $COMMAND
+echo "Starting server ..."
+screen -dmS ${SCREEN} ${JAVA} -Xmx${RAM} -Xms${RAM} ${FLAGS} -jar ${JAR} --nogui
