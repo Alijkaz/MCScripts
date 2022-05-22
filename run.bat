6490e261e0da5d309f784ae5a0ef7193f63b8de2
Ali J Kazemi <alijafarkazemi@gmail.com>
2022-05-22 09:26:29 +0430

    Update default values

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
 @echo off
 
-title Server
+title name
 
-set JAR=Server.jar
+set JAR=software.jar
 set RAM=1024M
-set JAVA="Java-Path\bin\java.exe"
+set JAVA="java" :: Can be changed to Java path for running server with specific Java installation
 set FLAGS=-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true -Dusing.aikars.flags=https://mcflags.emc.gs
 
 %JAVA% -Xmx%RAM% -Xms%RAM% %FLAGS% -jar %JAR% --nogui
