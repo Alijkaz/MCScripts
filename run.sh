f3c811284735c736b1cae7b2a42f36a6ca6aafde
Alijk <34511430+Alijkaz@users.noreply.github.com>
2022-08-02 16:50:10 +0430

    Update run.sh add server name on starting message

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ RAM="1024M"
 JAVA="java" # Can be changed to Java path for running server with specific Java installation
 FLAGS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true -Dusing.aikars.flags=https://mcflags.emc.gs"
 
-echo "Starting server ..."
+echo "Starting $SCREEN ..."
 screen -dmS ${SCREEN} ${JAVA} -Xmx${RAM} -Xms${RAM} ${FLAGS} -jar ${JAR} --nogui
